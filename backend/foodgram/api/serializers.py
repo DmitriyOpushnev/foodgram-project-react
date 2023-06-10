6c07ba5497d4496b2bc72449de1b7cf475344733
Дмитрий Опушнев <d.opushnev@yandex.ru>
2023-06-10 14:23:51 +0200

    WorkFlowTest

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,8 +1,6 @@
-import base64
-
-from django.core.files.base import ContentFile
 from django.shortcuts import get_object_or_404
 from djoser.serializers import UserSerializer
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers, status
 from rest_framework.exceptions import ValidationError
 from rest_framework.fields import SerializerMethodField
@@ -132,16 +130,6 @@ class IngredientRecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = AmountIngredients
         fields = ('id', 'name', 'measurement_unit', 'amount', )
-
-
-class Base64ImageField(serializers.ImageField):
-    """Кастомный сериализатор для поля ImageField."""
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-        return super().to_internal_value(data)
 
 
 class RecipeReadSerializer(serializers.ModelSerializer):
